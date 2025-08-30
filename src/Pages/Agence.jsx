ce7a7c7f12bdba357b225aabc3c5ce1cd4da872d
MasadRayan <masadrayan2002@gmail.com>
2025-08-30 13:49:50 +0600

    Agence added

--- src/Pages/Agence.jsx
+++ src/Pages/Agence.jsx
@@ -35,7 +35,7 @@ const Agence = () => {
                 start: 'top 28%',
                 end: 'top -100%',
                 pin: true,
-                scrub:true, 
+                scrub: true,
                 pinSpacing: true,
                 pinType: 'transform',
                 anticipatePin: 1,
@@ -54,25 +54,36 @@ const Agence = () => {
 
     return (
         <div className=''>
-            <div className='relative py-1'>
-                <div ref={imageDivRef} className='absolute top-100 left-[30vw] h-[20vw] w-[15vw] rounded-4xl overflow-hidden'>
-                    <img ref={imageRef} className='h-full w-full object-cover' src={p1} alt="" />
+            <div id='page1' className='py-1 '>
+                <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-96 -top-80 lg:left-[30vw] left-[30vw]'>
+                    <img ref={imageRef} className='h-full object-cover w-full' src={p1} alt="" />
                 </div>
-                <div className='font-[font1] relative'>
-                    <div className='pt-[45vh]'>
+                <div className='relative font-[font2]'>
+                    <div className='lg:mt-[55vh] mt-[30vh]'>
                         <h1 className='text-[20vw] text-center leading-[18vw]'>SEVEN7Y <br />
                             TWO</h1>
                     </div>
-                    <div className='pl-[40%] mt-10'>
-                        <p className='text-[50px] leading-tight '>
-                            &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.
-                        </p>
+                    <div className='lg:pl-[40%] lg:mt-20 mt-4 p-3'>
+                        <p className='lg:text-6xl text-xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
                     </div>
                 </div>
             </div>
             <div className='font-[font2] text-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-[12vw] container mx-auto'>
-                <div className='text-center flex-1'><h2>Expertise</h2></div>
-                <div className='flex justify-center mb-30'>
+
+                <div className='flex gap-20 justify-start items-start lg:hidden'>
+                    <div className=' pl-10'><h2>Expertise</h2></div>
+                    <div className=''>
+                        <div className='flex flex-col items-start'>
+                            <p>Strategy</p>
+                            <p>Advertising</p>
+                            <p>Branding</p>
+                            <p>Design</p>
+                            <p>Content</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='text-center flex-1 hidden lg:block'><h2>Expertise</h2></div>
+                <div className=' justify-center lg:mb-30 hidden lg:block'>
                     <div className='flex flex-col items-start'>
                         <p>Strategy</p>
                         <p>Advertising</p>
@@ -96,9 +107,7 @@ const Agence = () => {
                     </p>
                 </div>
             </div>
-            <div className='section-2 h-screen'>
 
-            </div>
         </div>
     );
 };
